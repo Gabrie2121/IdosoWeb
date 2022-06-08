af7457d3e6274bdfded2b3abf7dde0baab28e9a0
miguelmoreiraa <miguel.moreira.mm44@gmail.com>
2022-06-08 19:46:21 -0300

    Header com logo

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,21 +1,46 @@
 import React from "react";
-import { Link } from "react-router-dom";
-//  import logoIdoso from ".../assets/logo.png"
+import logoIdoso from '../../assets/logo.png'
+
+import styled from 'styled-components'
+
+
+const NavBar = styled.nav`
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+    background: #5BB159;
+    height: 8vh;
+`
+const Opcoes = styled.div`
+    list-style: none;
+    display: flex;
+    margin-left: 50%;
+`
+
+const OpcoesHeader = styled.li`
+    letter-spacing: 3px;
+    margin-left: 32px;
+`
+
+const Logo = styled.img`
+    width: 12%;
+    display: flex;
+`
+
 
 
 function Header() {
     return (
         <header>
-            <nav>
-                <Link className="logo" to="/" />
-                {/* <img src={logoIdoso} /> */}
-                <ul className="nav_list">
-                    <li>QUEM SOMOS</li>
-                    <li>CONTATO</li>
-                    <li>LOGIN</li>
-                    <li>CADASTRO</li>
-                </ul>
-            </nav>
+            <NavBar>
+                <Logo src={logoIdoso} />
+                <Opcoes>
+                    <OpcoesHeader>QUEM SOMOS</OpcoesHeader>
+                    <OpcoesHeader>CONTATO</OpcoesHeader>
+                    <OpcoesHeader>LOGIN</OpcoesHeader>
+                    <OpcoesHeader>CADASTRO</OpcoesHeader>
+                </Opcoes>
+            </NavBar>
         </header>
     );
 }
--- src/pages/HomeInstitucional.js
+++ src/pages/HomeInstitucional.js
@@ -1,12 +1,38 @@
 import React from "react";
 import Header from "../components/Header/";
 
-import '../styles/components/Header/styles.css'
+import styled from 'styled-components'
+
+import logoIdoso from '../assets/fotohome.png'
+
+const DivTodoFoto = styled.div`
+    width: 100vw;
+    height: 100vh;
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    align-items: center;
+`
+
+const DivFoto = styled.div`
+    width: 60%;
+    height: 80%;
+`
+const Imagem = styled.img`
+    width: fit-content;
+`
+
 
 function HomeInstitucional() {
     return (
         <div>
-           <Header />
+            <Header />
+            <DivTodoFoto>
+                <DivFoto>
+                    <Imagem src={logoIdoso}/>
+                </DivFoto>
+            </DivTodoFoto>
+
         </div>
     );
 }
