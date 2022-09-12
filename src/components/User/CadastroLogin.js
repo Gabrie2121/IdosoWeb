7796568cd1d9dd43a32f055b1016e6a1e98a5070
miguelmoreiraa <miguel.moreira.mm44@gmail.com>
2022-09-12 19:46:10 -0300

    Auto Format

--- src/components/User/CadastroLogin.js
+++ src/components/User/CadastroLogin.js
@@ -21,9 +21,9 @@ import { useAuth } from '../../providers/auth';
 const ImageUser = styled.img`
     width: 25%;
     height: 25%;
-    borderRadius: 100;
-    borderColor: "white";
-    borderWidth:1;
+    border-radius: 100;
+    border-color: "white";
+    border-width:1;
 `
 function FormCadastro() {
 
@@ -33,7 +33,7 @@ function FormCadastro() {
         Email: Usuario.Email,
         Password: Usuario.Password,
         ConfirmPassword: Usuario.ConfirmPassword,
-        NotifyEmail:Usuario.NotifyEmail,
+        NotifyEmail: Usuario.NotifyEmail,
         Nome: Usuario.Nome,
         Sobrenome: Usuario.Sobrenome,
         CPF: Usuario.CPF,
@@ -46,7 +46,7 @@ function FormCadastro() {
         Complemento: Usuario.Complemento,
         Apelido: Usuario.Apelido,
     });
-    
+
 
     const [valuesPassword, setPassword] = useState({
         showPassword: false,
@@ -113,16 +113,16 @@ function FormCadastro() {
         const { name, value } = ev.target;
 
         if ([name] === 'email') {
-            setInput({ ...input, Email:value });            
+            setInput({ ...input, Email: value });
         }
-        else if([name]==='password'){
-            setInput({  ...input, Password:value });
+        else if ([name] === 'password') {
+            setInput({ ...input, Password: value });
         }
-        else if([name]==='confirmPassword'){
-            setInput({  ...input,ConfirmPassword:value });
+        else if ([name] === 'confirmPassword') {
+            setInput({ ...input, ConfirmPassword: value });
         }
-        else if([name]==='notifyEmail'){
-            setInput({ ...input, NotifyEmail:value });
+        else if ([name] === 'notifyEmail') {
+            setInput({ ...input, NotifyEmail: value });
         }
         setUsuario(input);
         localStorage.setItem('Usuario', JSON.stringify(input));
@@ -130,62 +130,62 @@ function FormCadastro() {
 
     return (
         <div>
-                <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
-                    <ImageUser src={foto} />
-                    <TextField name="email" required id="outlined-required" label="Alterar E-mail"  onChange={onChange} value={(input.Email)} />
-                    <FormGroup>
-                        <FormControlLabel control={<Checkbox name="notifyEmail" defaultChecked color="success"  onChange={onChange} value={(input.NotifyEmail)} />} label="Receber notificações no e-mail" />
-                    </FormGroup>
-                    <FormControl sx={{ m: 1, width: '25ch' }} variant="outlined">
-                        <InputLabel htmlFor="outlined-adornment-password">Senha</InputLabel>
-                        <OutlinedInput
-                            name="password"
-                            id="outlined-adornment-password"
-                            type={valuesPassword.showPassword ? 'text' : 'password'}
-                            onChange={onChange} 
-                            onBlur={validateprops}
-                            value={(input.Password)}
-                            endAdornment={
-                                <InputAdornment position="end">
-                                    <IconButton
-                                        aria-label="toggle password visibility"
-                                        onClick={handleClickShowPassword}
-                                        onMouseDown={handleMouseDownPassword}
-                                        edge="end"
-                                    >
-                                        {valuesPassword.showPassword ? <VisibilityOff /> : <Visibility />}
-                                    </IconButton>
-                                </InputAdornment>
-                            }
-                            label="Password"
-                        />
-                    </FormControl>
-                    <FormControl sx={{ m: 1, width: '25ch' }} variant="outlined">
-                        <InputLabel htmlFor="outlined-adornment-Confirmpassword">Confirmar Senha</InputLabel>
-                        <OutlinedInput
-                            name="confirmPassword"
-                            id="outlined-adornment-Confirmpassword"
-                            type={valuesPassword.showconfirmPassword ? 'text' : 'password'}
-                            onChange={onChange}
-                            onBlur={validateprops}
-                            value={(input.ConfirmPassword)}
-                            endAdornment={
-                                <InputAdornment position="end">
-                                    <IconButton
-                                        aria-label="toggle password visibility"
-                                        onClick={handleClickShowConfirmPassword}
-                                        onMouseDown={handleMouseDownConfirmPassword}
-                                        edge="end"
-                                    >
-                                        {valuesPassword.showconfirmPassword ? <VisibilityOff /> : <Visibility />}
-                                    </IconButton>
-                                </InputAdornment>
-                            }
-                            label="confirmPassword"
-                        />
-                    </FormControl>
-                    {error.confirmPassword && <span className='err'>{error.confirmPassword}</span>}
-                </Box>
+            <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+                <ImageUser src={foto} />
+                <TextField name="email" required id="outlined-required" label="Alterar E-mail" onChange={onChange} value={(input.Email)} />
+                <FormGroup>
+                    <FormControlLabel control={<Checkbox name="notifyEmail" defaultChecked color="success" onChange={onChange} value={(input.NotifyEmail)} />} label="Receber notificações no e-mail" />
+                </FormGroup>
+                <FormControl sx={{ m: 1, width: '25ch' }} variant="outlined">
+                    <InputLabel htmlFor="outlined-adornment-password">Senha</InputLabel>
+                    <OutlinedInput
+                        name="password"
+                        id="outlined-adornment-password"
+                        type={valuesPassword.showPassword ? 'text' : 'password'}
+                        onChange={onChange}
+                        onBlur={validateprops}
+                        value={(input.Password)}
+                        endAdornment={
+                            <InputAdornment position="end">
+                                <IconButton
+                                    aria-label="toggle password visibility"
+                                    onClick={handleClickShowPassword}
+                                    onMouseDown={handleMouseDownPassword}
+                                    edge="end"
+                                >
+                                    {valuesPassword.showPassword ? <VisibilityOff /> : <Visibility />}
+                                </IconButton>
+                            </InputAdornment>
+                        }
+                        label="Password"
+                    />
+                </FormControl>
+                <FormControl sx={{ m: 1, width: '25ch' }} variant="outlined">
+                    <InputLabel htmlFor="outlined-adornment-Confirmpassword">Confirmar Senha</InputLabel>
+                    <OutlinedInput
+                        name="confirmPassword"
+                        id="outlined-adornment-Confirmpassword"
+                        type={valuesPassword.showconfirmPassword ? 'text' : 'password'}
+                        onChange={onChange}
+                        onBlur={validateprops}
+                        value={(input.ConfirmPassword)}
+                        endAdornment={
+                            <InputAdornment position="end">
+                                <IconButton
+                                    aria-label="toggle password visibility"
+                                    onClick={handleClickShowConfirmPassword}
+                                    onMouseDown={handleMouseDownConfirmPassword}
+                                    edge="end"
+                                >
+                                    {valuesPassword.showconfirmPassword ? <VisibilityOff /> : <Visibility />}
+                                </IconButton>
+                            </InputAdornment>
+                        }
+                        label="confirmPassword"
+                    />
+                </FormControl>
+                {error.confirmPassword && <span className='err'>{error.confirmPassword}</span>}
+            </Box>
         </div>
     );
 }
